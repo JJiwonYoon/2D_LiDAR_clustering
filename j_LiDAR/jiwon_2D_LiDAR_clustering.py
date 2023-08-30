03c9ac8fa6a96f45c160316975ec4307cd9ab7a9
JJiwonYoon <wldnjs0488@gmail.com>
2023-08-30 17:08:43 +0900

    split two list

--- j_LiDAR/jiwon_2D_LiDAR_clustering.py
+++ j_LiDAR/jiwon_2D_LiDAR_clustering.py
@@ -10,17 +10,25 @@ import math
 class clustering(Node):
     def __init__(self):
         super().__init__('clustering_node')
-        self.x_LiDAR        = 200
+        self.x_LiDAR        = 200 #mm
         self.y_LiDAR        = 300
         self.cnt            = 0
-        self.distance_data  = []
-        self.distance       = 0
+        self.distance       = 0   #mm
         self.RC_group = ReentrantCallbackGroup()
         self.create_subscription(LiDAR,'/scan',self.LiDAR_callback,1, callback_group=self.RC_group)
 
+    def split_into_pairs(self,lst):
+        pairs = [lst[i:i+2] for i in range(0, len(lst) - 1, 2)]
+        if len(lst) % 2 == 1:
+            pairs[-1:1].append([lst[-1]])
+        if len(pairs) == 0:
+            pass
+        return pairs
+
     def LiDAR_callback(self,msg):
-        
         self.converted_data = []
+        self.distance_data  = []
+        self.filtered_data  = []
         self.lidar_data = msg.ranges
         self.lidar_angle_increment = msg.angle_increment
         for i in range(len(self.lidar_data)):
@@ -30,27 +38,28 @@ class clustering(Node):
 
             if (abs(x) < self.x_LiDAR) & (y > 0) & (y < self.y_LiDAR): #mm
                 self.cnt +=1
-                self.converted_data.append([x,y,self.cnt])
+                self.converted_data.append([x,y])
                 self.distance = (math.sqrt(x*x + y*y))
                 self.distance_data.append(self.distance)
-                # print(f"{(self.distance_data)}")
-                for i in range(len(self.distance_data)):
-                    if abs(self.distance_data[i] - self.distance_data[i-1]) > 100:
-                        print(f'{self.distance_data}')
-                    else:
-                        pass
+                pairs = self.split_into_pairs(self.distance_data)
+                print(f"{(pairs)}")
+
+                # for i in range(len(self.distance_data)):
+
+                #     if abs(self.distance_data[i] - self.distance_data[i-1]) > 100:
+                #         print(f'{self.distance_data}')
+                #     else:
+                #         pass
+                # 
 
 def main():
-    rclpy.init()
-
-    C = clustering()
-
-    try:
-        rclpy.spin(C)
-
-    finally:
-        C.destroy_node()
-        rclpy.shutdown()
+        rclpy.init()
+        C = clustering()
+        try:
+            rclpy.spin(C)
+        finally:
+            C.destroy_node()
+            rclpy.shutdown()
 
 if __name__ == '__main__':
     main()

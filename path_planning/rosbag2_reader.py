c43d0cd31ce54839465ef7f5253da2ab5d88f037
JJiwonYoon <wldnjs0488@gmail.com>
2023-09-01 11:23:38 +0900

    ros2bag_gps_parsing

--- path_planning/rosbag2_reader.py
+++ path_planning/rosbag2_reader.py
@@ -10,31 +10,30 @@ class Text_Reader(Node):
     def __init__(self):
         super().__init__('Text_Reader')
         self.subscription = self.create_subscription(SbgGpsPos,'/sbg/gps_pos',self.gps_callback,10)
+        self.utm_x=[]
+        self.utm_y=[]
 
     def gps_callback(self, msg):
         self.gps_x = float(msg.latitude)
-        print(f"{self.gps_x}")
+        self.gps_y = float(msg.latitude)
+        self.utm_position = utm.from_latlon(self.gps_x,self.gps_y)
+        self.utm_x.append(self.utm_position[0])
+        self.utm_y.append(self.utm_position[1])
+        self.save_path('/home/labmas/robot_ws/src/j_LiDAR/path', "gps", self.utm_x, self.utm_y)
 
-    def save_path(self,directory, sensor_name, x = [], y = [], yaw = []):
+    def save_path(self,directory, sensor_name, x = [], y = []):
         f_x   = open(directory + "/" + sensor_name + "_x.txt",   'w')
         f_y   = open(directory + "/" + sensor_name + "_y.txt",   'w')
-        f_yaw = open(directory + "/" + sensor_name + "_yaw.txt", 'w')
 
         for i in range(0, len(x)-1):
             f_x.write(str(x[i]) + '\n')
             f_y.write(str(y[i]) + '\n')
-            f_yaw.write(str(yaw[i]) + '\n')
 
         f_x.write(str(x[-1]))
         f_y.write(str(y[-1]))
-        f_yaw.write(str(yaw[-1]))
 
         f_x.close()
         f_y.close()
-        f_yaw.close()
-        
-        def xy_utm_conversion(self):
-            pass
 
 def main(args=None):
     rclpy.init(args=args)
